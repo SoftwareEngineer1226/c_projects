6464a0a18bb6f455502863d445f0b5bbb7b2f585
levrek <huseyinunlu37@gmail.com>
2023-10-23 15:01:45 +0300

    cycle detection changed

--- Parallel_Make/parmake.c
+++ Parallel_Make/parmake.c
@@ -6,36 +6,26 @@
 
 
 
-int parmake_h(graph* dependency_graph, char* val, vector* visited){
+int parmake_h(graph* dependency_graph, char* val){
 
-    short hasCycle = 0;
-    VECTOR_FOR_EACH(visited, temp, {
-        if(temp == val) {
-            print_cycle_failure(val);
-            hasCycle = 1;
-        }
+
+    vector* neighbors = graph_neighbors(dependency_graph, val);
+
+    void **_it = vector_begin(neighbors);
+    void **_iend = vector_end(neighbors);
+    _iend--;
+    for (; _it <= _iend; _iend--) {
+        char* val_n = *_iend;
+        int res = parmake_h(dependency_graph, val_n);
         
-    });
+        if(res == 0){
+            vector_destroy(neighbors);
+            return 0;
+        }
+    }   
 
-    if(!hasCycle){
-        vector_push_back(visited, val);
-        vector* neighbors = graph_neighbors(dependency_graph, val);
-
-        void **_it = vector_begin(neighbors);
-        void **_iend = vector_end(neighbors);
-        _iend--;
-        for (; _it <= _iend; _iend--) {
-            char* val_n = *_iend;
-            int res = parmake_h(dependency_graph, val_n, visited);
-            
-            if(res == 0){
-                vector_destroy(neighbors);
-                return 0;
-            }
-        }   
-        vector_destroy(neighbors);
-
-    }    
+    vector_destroy(neighbors);
+                
     rule_t* rule = graph_get_vertex_value(dependency_graph, val);
 
     vector* commands = rule->commands;
@@ -48,24 +38,48 @@ int parmake_h(graph* dependency_graph, char* val, vector* visited){
     return 1;
 }
 
+int hasCycle(graph* graph, char* dependency, vector* visited){
+    VECTOR_FOR_EACH(visited, _temp,{
+        if(strcmp((char*)_temp, dependency) == 0){
+            return 1;
+        }
+    });
+    vector_push_back(visited, dependency);
+
+    vector* neighbors = graph_neighbors(graph, dependency);
+    VECTOR_FOR_EACH(neighbors, neighbor,{
+        int res = hasCycle(graph, neighbor, visited);
+        if(res == 1){
+            return 1;
+        }
+    });
+    return 0;
+}
+
 int parmake(char *makefile, size_t num_threads, char **targets) {
 
     graph* dependency_graph = parser_parse_makefile(makefile, targets);
     if(dependency_graph == NULL) return 0;
-    vector* vertices = graph_vertices(dependency_graph);
-    if(vector_empty(vertices)) return 0;
+    int size = graph_vertex_count(dependency_graph);
+    if(size == 0) return 0;
 
     vector* target_vertices = graph_neighbors(dependency_graph, "");
-    
-    VECTOR_FOR_EACH(target_vertices, vertice, {
-        vector* visited = vector_create(NULL, NULL, NULL);
-        parmake_h(dependency_graph, vertice, visited);
-        vector_destroy(visited);
+    vector* visited_vertices;
+    VECTOR_FOR_EACH(target_vertices, vertex, {
+        visited_vertices = vector_create(NULL, NULL, NULL);
+        int _hasCycle = hasCycle(dependency_graph, vertex, visited_vertices);
+
+        vector_destroy(visited_vertices);
+        if(_hasCycle){
+            print_cycle_failure(vertex);
+        }
+        else{
+            parmake_h(dependency_graph, vertex);
+        }
     });
 
     
     vector_destroy(target_vertices);
-    vector_destroy(vertices);
     graph_destroy(dependency_graph);
 
     return 1;
