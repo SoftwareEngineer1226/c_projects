34eacd1d76db0ca52151622fe65e02e4fc436c2f
levrek <huseyinunlu37@gmail.com>
2023-11-05 01:41:27 +0300

    initial commit

--- finding_filesystems/minixfs.c
+++ finding_filesystems/minixfs.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * Virtual paths:
@@ -33,24 +34,97 @@ int minixfs_virtual_path_count =
     sizeof(minixfs_virtual_path_names) / sizeof(minixfs_virtual_path_names[0]);
 
 int minixfs_chmod(file_system *fs, char *path, int new_permissions) {
-    // Thar she blows!
+    inode* i_node = get_inode(fs, path);
+    if(i_node == NULL){
+        errno = ENOENT;
+        return -1;
+    }
+
+    int mask = (1 << 2) - 1;
+    i_node->mode = (new_permissions << 2) | (i_node->mode & mask);
     return 0;
 }
 
 int minixfs_chown(file_system *fs, char *path, uid_t owner, gid_t group) {
-    // Land ahoy!
-    return -1;
+    inode* i_node = get_inode(fs, path);
+    if(i_node == NULL){
+        errno = ENOENT;
+        return -1;
+    }
+    if(owner != (u_int)-1){
+        i_node->uid = owner;
+    }
+    if(group != (u_int)-1){
+        i_node->gid = group;
+    }
+
+    return 0;
 }
 
 inode *minixfs_create_inode_for_path(file_system *fs, const char *path) {
-    // Land ahoy!
-    return NULL;
+    inode* i_node = get_inode(fs, path);
+    if(i_node != NULL){
+        return NULL;
+    }
+    if(!valid_filename(path)){
+        return NULL;
+    }
+    
+    int len = (int)strlen(path);
+    const char *endptr = path + len;
+    while (*endptr != '/') {
+        endptr--;
+    }
+
+    clock_gettime(CLOCK_REALTIME, &(i_node->atim));
+    clock_gettime(CLOCK_REALTIME, &(i_node->atim));
+
+
+    char* filename = (char*)(endptr + 1);
+    char *parent_path = malloc(endptr - path + strlen("/") + 1);
+    strncpy(parent_path, path, endptr - path + 1);
+    parent_path[endptr - path + 1] = '\0';
+    inode *parent_node = get_inode(fs, parent_path);
+
+    inode* ret_node = (inode*) malloc(sizeof(inode));
+    init_inode(parent_node, ret_node);
+
+    minixfs_dirent dirent;
+    dirent.name = filename;
+    size_t inode_number = ret_node - fs->inode_root;
+    dirent.inode_num = inode_number;
+
+    make_string_from_dirent(fs->data_root[ret_node->direct[0]].data, dirent);
+
+
+    return ret_node;
+}
+
+size_t get_used_datablock_count(file_system* fs){
+
+    size_t used_datablocks = 0;
+
+    for(int i=0;i<(int)fs->meta->inode_count;i++){
+        inode* _inode = (fs->inode_root + (i*sizeof(inode)));
+        for(int j = 0; j<NUM_DIRECT_BLOCKS;j++){
+            if(_inode->direct[j] > 0){
+                used_datablocks++;
+            }
+        }
+        if(_inode->indirect > 0){
+            used_datablocks++;
+        }
+    }
+    return used_datablocks;
 }
 
 ssize_t minixfs_virtual_read(file_system *fs, const char *path, void *buf,
                              size_t count, off_t *off) {
     if (!strcmp(path, "info")) {
-        // TODO implement the "info" virtual file here
+        size_t used = get_used_datablock_count(fs);
+        char* string_to_print = block_info_string(used);
+        printf("%s", string_to_print);
+        return 0;
     }
 
     errno = ENOENT;
@@ -59,8 +133,15 @@ ssize_t minixfs_virtual_read(file_system *fs, const char *path, void *buf,
 
 ssize_t minixfs_write(file_system *fs, const char *path, const void *buf,
                       size_t count, off_t *off) {
-    // X marks the spot
-    return -1;
+    minixfs_create_inode_for_path(fs, path);
+    return 0;
+}
+
+void print_buffer(void* buf, size_t size){
+    for(size_t i=0;i<size;i++){
+        printf("%c", *(char*)(buf+i));
+    }
+    
 }
 
 ssize_t minixfs_read(file_system *fs, const char *path, void *buf, size_t count,
@@ -68,6 +149,56 @@ ssize_t minixfs_read(file_system *fs, const char *path, void *buf, size_t count,
     const char *virtual_path = is_virtual_path(path);
     if (virtual_path)
         return minixfs_virtual_read(fs, virtual_path, buf, count, off);
-    // 'ere be treasure!
-    return -1;
+    
+    inode* i_node = get_inode(fs, path);
+    if(i_node == NULL){
+        errno = ENOENT;
+        return -1;
+    }
+
+    clock_gettime(CLOCK_REALTIME, &(i_node->atim));
+
+    if (*off >= (long)i_node->size) {
+        return 0;
+    }
+
+    size_t bytes_remain = i_node->size;
+
+    buf =  (void *)calloc(i_node->size, sizeof(char));
+    for (int i = 0; i < NUM_DIRECT_BLOCKS; i++) {
+        data_block_number block_number = i_node->direct[i];
+        if (block_number > 0) {
+            data_block data_block = fs->data_root[block_number];
+            size_t data_size = sizeof(data_block.data);
+            if(data_size > bytes_remain){
+                data_size = bytes_remain;
+            }
+            memcpy(buf + *off, data_block.data, sizeof(data_block.data));
+            
+            *off += data_size;
+            bytes_remain -= data_size;
+            if(bytes_remain == 0)break;
+        }
+    }
+
+    data_block_number indirect_block_number = i_node->indirect;
+    data_block indirect_block = fs->data_root[indirect_block_number];
+
+    for (u_long i = 0; i < NUM_INDIRECT_BLOCKS; i++) {
+        data_block_number block_number = indirect_block.data[i];
+        if (block_number > 0) {
+            data_block data_block = fs->data_root[block_number];
+            size_t data_size = sizeof(data_block.data);
+            if(data_size > bytes_remain){
+                data_size = bytes_remain;
+            }
+            memcpy(buf + *off, data_block.data, data_size);
+            *off += data_size;
+            bytes_remain -= data_size;
+            if(bytes_remain == 0)break;
+
+        }
+    }
+    print_buffer(buf, i_node->size);
+    return 0;  
 }
