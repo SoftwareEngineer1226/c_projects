ed711c4d0262f4f974140c3db157fa26a534c38b
Fraser Orr <fraserorr@gmail.com>
2023-11-08 18:21:28 -0600

    Network project

---
+++ nonstop_networking/common.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <stddef.h>
+#include <sys/types.h>
+
+#define LOG(...)                      \
+    do {                              \
+        fprintf(stderr, __VA_ARGS__); \
+        fprintf(stderr, "\n");        \
+    } while (0);
+
+typedef enum { GET, PUT, DELETE, LIST, V_UNKNOWN } verb;
