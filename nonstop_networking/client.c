8c77102490a8a684a9ccea4ddc617239cde84585
levrek <huseyinunlu37@gmail.com>
2023-11-12 04:05:59 +0300

    first push

--- nonstop_networking/client.c
+++ nonstop_networking/client.c
@@ -5,13 +5,128 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
 #include "common.h"
 
+#define MAX_BUFFER_SIZE 1024
+
 char **parse_args(int argc, char **argv);
 verb check_args(char **args);
+
+
+
+int put_file_into_buffer(char* buffer, char* filename, size_t* off){
+    FILE *file = fopen(filename, "rb");
+
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    rewind(file);
+    
+    size_t elements_to_send = (size_t)((file_size < MAX_BUFFER_SIZE - (long)*off) ? file_size : MAX_BUFFER_SIZE - *off);
+    
+    memcpy(buffer + *off, &elements_to_send, sizeof(size_t));
+    *off += sizeof(size_t);
+    size_t elements_read = fread(buffer+*off, 1, elements_to_send, file);
+    *off += elements_read;
+    memcpy(buffer + *off, "\0", 1);
+
+    if (elements_read != elements_to_send) {
+        perror("Error reading from the input file");
+        fclose(file);
+        return -1;
+    }
+    
+    fclose(file);
+    return 0;
+}
+
+void create_message(char* buffer, char* verb_as_char, char* filename, size_t* off){
+    memcpy(buffer, verb_as_char, strlen(verb_as_char));
+    *off += strlen(verb_as_char);
+    
+    if(filename != NULL){
+        memcpy(buffer + *off, " ", 1);
+        *off+=1;
+        memcpy(buffer + *off, filename, strlen(filename));
+        *off+=strlen(filename);
+    }
+    
+    memcpy(buffer + *off, "\n\0", 2);
+    *off+=2;
+}
+
 int main(int argc, char **argv) {
-    // Good luck!
+    char** args = parse_args(argc, argv);
+    if(args == NULL){
+        print_client_help();
+        return -1;
+    }
+    char* host = args[0];
+    int port = atoi(args[1]);
+    char* verb_as_char = args[2];
+    verb _verb = check_args(args);
+    char* remotefile = NULL;
+    char* localfile = NULL;
+    if(argc > 3){
+        remotefile = args[3];
+    }
+    if(argc > 4){
+        localfile = args[4];
+    }
+
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[MAX_BUFFER_SIZE] = {0};
+
+    // Creating a socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        print_error_message("Socket creation error");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, host, &serv_addr.sin_addr) <= 0) {
+        print_error_message("Invalid address/ Address not supported");
+        return -1;
+    }
+
+    // Connecting to the server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        print_error_message("Connection failed");
+        return -1;
+    }
+
+    size_t off = 0;
+    create_message(buffer, verb_as_char, remotefile, &off);
+    // Sending the message to the server
+    if(_verb == PUT){
+        put_file_into_buffer(buffer, remotefile, &off);
+
+    }
+
+    if (send(sock, buffer, off, 0) < 0) {
+        print_error_message("Error sending message");
+        return -1;
+    }
+
+    // Receiving the message from the server
+    if (read(sock, buffer, 1024) < 0) {
+        print_error_message("Read error");
+        return -1;
+    }
+
+    
+    
+    printf("%s\n", buffer);
+
+    // Close the socket
+    close(sock);
+
+    return 0;
 }
 
 /**
--- nonstop_networking/server.c
+++ nonstop_networking/server.c
@@ -1,4 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#include "common.h"
+#include "format.h"
+
+
+#define PORT 8080
+
+#define MAX_BUFFER_SIZE 1024
+
+void printBuffer(const char *buffer, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        printf("%02X ", (unsigned char)buffer[i]); // Prints in hexadecimal format
+    }
+    printf("\n");
+}
+
 int main(int argc, char **argv) {
-    // good luck!
+    int server_fd, sock;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char *message = "Hello from the server!";
+    char buffer[MAX_BUFFER_SIZE];
+    int bytesRead;
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Binding to the specified port
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        print_error_message("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listening for incoming connections
+    if (listen(server_fd, 3) < 0) {
+        print_error_message("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Accepting the incoming connection
+    if ((sock = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
+        print_error_message("Accept failed");
+        exit(EXIT_FAILURE);
+    }
+    bytesRead = recv(sock, buffer, MAX_BUFFER_SIZE, 0);
+    if (bytesRead < 0) {
+        perror("Error receiving data");
+        exit(EXIT_FAILURE);
+    }
+
+    buffer[bytesRead] = '\0'; // Null-terminate the received data
+
+    printBuffer(buffer, MAX_BUFFER_SIZE);
+    // Sending a message to the client
+    send(sock, message, strlen(message), 0);
+    printf("Message sent to the client\n");
+
+    close(sock);
+
+    return 0;
 }
